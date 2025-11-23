3ba9dafd37e98ae333ecf11d74f181e55085908d
Infixo <swidwins@gmail.com>
2025-11-23 22:34:38 +0100

    Adds source methods analyzer for easier patch maintenance.

--- DTTweaksMod.cs
+++ DTTweaksMod.cs
@@ -1,6 +1,4 @@
-﻿#pragma warning disable CA1416 // Validate platform compatibility
-
-using HarmonyLib;
+﻿using HarmonyLib;
 using STM.Data;
 using STM.Data.Entities;
 using STM.GameWorld;
@@ -14,52 +12,23 @@ namespace DTTweaks;
 
 public static class ModEntry
 {
-    public static string ModName { get; private set; } = nameof(DTTweaks);
-    public static string HarmonyId { get; private set; } = String.Empty;
-
+    public static readonly string ModName = nameof(DTTweaks);
 
     [UnmanagedCallersOnly]
     public static int InitializeMod()
     {
-        DebugConsole.Show();
-
-        // Gather environment info
-        Assembly currentAssembly = Assembly.GetExecutingAssembly();
-        ModName = currentAssembly.GetName().Name ?? nameof(DTTweaks);
-        string? assemblyDirectory = Path.GetDirectoryName(currentAssembly.Location);
-        HarmonyId = "Infixo." + ModName;
-        Log.Write($"Mod {ModName} successfully started. HarmonyId is {HarmonyId}.");
-
-        // Read config data
-        _ = ConfigToolXml.LoadConfig(ModName, assemblyDirectory!);
-
-        try
+        if (ModInit.InitializeMod(ModName))
         {
-            // Harmony
-            var harmony = new Harmony(HarmonyId);
-            //harmony.PatchAll(typeof(Mod).Assembly);
-            harmony.PatchAll();
-            var patchedMethods = harmony.GetPatchedMethods().ToArray();
-            Log.Write($"Plugin {HarmonyId} made patches! Patched methods: " + patchedMethods.Length);
-            foreach (var patchedMethod in patchedMethods)
-            {
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-                Log.Write($"Patched method: {patchedMethod.Module.Name}:{patchedMethod.DeclaringType.Name}.{patchedMethod.Name}");
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            }
+            // Gather environment info
+            Assembly currentAssembly = Assembly.GetExecutingAssembly();
+            string? assemblyDirectory = Path.GetDirectoryName(currentAssembly.Location);
+            // Read config data
+            _ = ConfigToolXml.LoadConfig(ModName, assemblyDirectory!);
+            // Do other stuff here to initialize
+            if (ConfigToolXml.Config?.Defaults != null) ChangeMainData();
+            Log.DumpMainDataDefaults();
+            if (ConfigToolXml.Config?.Vehicles != null) ChangeVehicles();
         }
-        catch (Exception ex)
-        {
-            Log.Write("EXCEPTION. ABORTING.");
-            Log.Write(ex.ToString());
-            return 2;
-        }
-
-        // do other stuff here to initialize
-        if (ConfigToolXml.Config?.Defaults != null) ChangeMainData();
-        Log.DumpMainDataDefaults();
-        if (ConfigToolXml.Config?.Vehicles != null) ChangeVehicles();
-
         return 0;
     }
 
@@ -77,34 +46,6 @@ public static class ModEntry
             else
                 Log.Write($"Wrong param: {param}");
         }
-
-        //MainData.Defaults.SetPublicProperty("Passenger_max_search_connections", 4); // 6
-
-        // Roads & Rails cost
-        //MainData.Defaults.SetPublicProperty("Road_price", 200000); // 150k => 200k
-        //MainData.Defaults.SetPublicProperty("Rails_price", 600000);  // 250k => 600k
-
-        // Station times
-        //MainData.Defaults.SetPublicProperty("Bus_station_time", 300); // 900
-        //MainData.Defaults.SetPublicProperty("Train_station_time", 600);  // 300
-        //Plane_airport_time = 3600(Int32)
-        //Ship_port_time = 3600(Int32)
-
-        // passengers per month: 86400 / City_destination_progress
-        // 1 would mean 1 passenger every second
-        // normal 5760 is 15, high 2880 is 30
-        // Max_level_destination=4  
-        //City_destination_progress_cap = 15(Int32) // destination from 15 to 60 passengers
-        //Utilities.Log.DumpMainDataDefaults: City_destination_progress= 5760(Int32)
-        // passengers high
-        //City_destination_progress_cap= 30(Int32) // destination from 30 to 120 passengers
-        //Utilities.Log.DumpMainDataDefaults: City_destination_progress= 2880(Int32)
-
-        // City shrinks if fulfillment is below that threshold
-        //MainData.Defaults.SetPublicProperty("City_shrink_threshold", 0.5m); // 0.3
-
-        // Last destination is changed if its fulfillment is below that threshold
-        //MainData.Defaults.SetPublicProperty("City_destination_change = 0,3(Decimal) // last destination is changed if below that
     }
 
     internal static void ChangeVehicles()
@@ -141,28 +82,6 @@ public static class ModEntry
             else
                 Log.Write($"Wrong vehicle {vehicle}");
         }
-
-        // Road vehicles
-        //RoadVehicleEntity vehicle = MainData.Road_vehicles.Where(x => x.Name == "vehicle_antero").First();
-        //vehicle.SetPublicProperty("Capacity", 20); // 3 Antero 12 => 20 // less cap than prev
-        //vehicle.SetPublicProperty("Price", 175000);
-
-        // Ships
-        //MainData.Ships.Where(x => x.Name == "vehicle_silver_tide").First().SetPublicProperty("Capacity", 700); // 5 Silver Tide 860 => 700
-        //MainData.Ships.Where(x => x.Name == "vehicle_royal_wanderer").First().SetPublicProperty("Capacity", 1460); // 6 Royal Wanderer  1360 => 1460
-        //MainData.Ships.Where(x => x.Name == "vehicle_sea_conqueror").First().SetPublicProperty("Capacity", 2800); // 6 Sea Conqueror 7600 => 2800 // absurd number
-        //MainData.Ships.Where(x => x.Name == "vehicle_wind_rider").First().SetPublicProperty("Capacity", 190); // 3 Wind Rider 250 => 190 // too many, makes next one worse
-
-        // Trains
-        //MainData.Trains.Where(x => x.Name == "vehicle_class_87").First().SetPublicProperty("Capacity", 420/6); // 5 Class 87 360 => 430 // lower capacity than previous!
-        //MainData.Trains.Where(x => x.Name == "vehicle_apex_3").First().SetPublicProperty("Capacity", 450/6); // 6 Apex 3 390 => 450 // only Tier6 with less cap than Tier5
-        //MainData.Trains.Where(x => x.Name == "vehicle_gsw_5").First().SetPublicProperty("Capacity", 320/5); // 4 GSW - 5 275 => 320 // less cap than prev 
-
-        // Planes
-        //MainData.Planes.Where(x => x.Name == "vehicle_a950l").First().SetPublicProperty("Capacity", 150); // 5 120 => 150 // lower capacity than previous!
-        //MainData.Planes.Where(x => x.Name == "vehicle_822_135").First().SetPublicProperty("Capacity", 135); // 5 134 => 135 stick with the name
-        //MainData.Planes.Where(x => x.Name == "vehicle_822_270").First().SetPublicProperty("Capacity", 270); // 5 134 => 270 stick with the name
-        //MainData.Planes.Where(x => x.Name == "vehicle_900_350").First().SetPublicProperty("Capacity", 350); // 5 351 => 350 stick with the name
 
         // Dump companies
         Log.Write("--- VEHICLE COMPANIES ---");
@@ -224,5 +143,3 @@ public static class Patches
         return __instance.Level < max;
     }
 }
-
-#pragma warning restore CA1416 // Validate platform compatibility
